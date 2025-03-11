3050b619e54c4dbd061390654bb46a469e8f2662
Jorge Perez Burgos <vaijira@gmail.com>
2025-03-11 20:50:25 +0100

    Change generate script

--- books/es/conferencias_miguel_anxo_bastos_vol_i/bookdown/generate.sh
+++ books/es/conferencias_miguel_anxo_bastos_vol_i/bookdown/generate.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env Rscript
+#!/bin/sh
 
-bookdown::clean_book()
-bookdown::render_book("index.Rmd", "bookdown::bs4_book")
-bookdown::render_book("index.Rmd", "bookdown::pdf_book")
-bookdown::render_book("index.Rmd", "bookdown::epub_book")
+## Get the path to the book
+BOOK_PATH=$1
+
+## Get the current path
+CURRENT_PATH=$(pwd)
+
+## Get to the right path
+cd ${BOOK_PATH}
+
+## Compile the book
+Rscript -e 'bookdown::clean_book()'
+Rscript -e 'bookdown::render_book("index.Rmd", output_format = "bookdown::bs4_book")'
+
+## Get back to the previous path
+cd ${CURRENT_PATH}
